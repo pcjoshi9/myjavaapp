1455d161f1ae3cde5094909527b3af4cdcced342
pcjoshi9 <pcjoshi9@gmail.com>
2020-11-19 13:48:54 -0800

    more checkins

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM java:8
+#RUN mkdir /home/core \
+#    cd /home/core \
+RUN git clone https://github.com/pcjoshi9/base-core.git
 COPY . /var/www/java
 WORKDIR /var/www/java
 RUN javac myapp.java
--- myapp.java
+++ myapp.java
@@ -1,6 +1,24 @@
+import java.io.*;
+import java.util.Scanner;
+import java.io.*;
+
 class myapp {
 public static void main(String[] args) {
 System.out.println("My sample app to test multi repo sharing common core code - PJ");
+
+int character;
+StringBuffer buffer = new StringBuffer("");
+try
+{
+FileInputStream inputStream = new FileInputStream(new File("/base-core/folder1/file1"));
+
+while( (character = inputStream.read()) != -1)
+        buffer.append((char) character);
+
+inputStream.close();
+System.out.println(buffer);
+}
+catch(IOException ex) { System.out.println("Error reading file" + ex); }
 }
 }
 
